29f6906eb58e76b890a8dadfcd2d4093bd52d1aa
horquidia-lima <horquidia.a.lima@gmail.com>
2023-12-09 17:06:35 -0300

    working with the method GET

--- src/server.js
+++ src/server.js
@@ -4,5 +4,9 @@ const express = require("express")
 //initialize express
 const app = express() 
 
+app.get("/", (request, response) => {
+  response.send("Hello World")
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
