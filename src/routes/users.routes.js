60383d6acc01ac8372a665be183b694b0714a7ec
horquidia-lima <horquidia.a.lima@gmail.com>
2023-12-16 14:56:20 -0300

    Utilizando Middlewares

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,17 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
+function myMiddleware(request, response, next){
+  console.log("Voce passou pelo Middleware")
+  if(!request.body.isAdmin){
+    return response.json({message: "user unauthorized"})
+  }
+  next()
+}
+
+
 const usersController = new UsersController //instaciando a class (guardando na memoria)
 
-usersRoutes.post("/", usersController.create)
+usersRoutes.post("/", myMiddleware, usersController.create)
 
 module.exports = usersRoutes
