be8dc05d268a7395a0f52145e98726b2750d45cc
horquidia-lima <horquidia.a.lima@gmail.com>
2024-01-12 07:54:19 -0300

    migration

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 
 //import express
@@ -7,13 +7,13 @@ const express = require("express")
 
 const routes = require("./routes")
 
+migrationsRun() //executar my DB
+
 //initialize express
 const app = express() 
 app.use(express.json())
 
 app.use(routes)
-
-database() //executar my DB
 
 /*app.get("/message/:id", (request, response) => {
   const {id} = request.params
