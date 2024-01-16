861f326bd03f13c535c021e13f13d70330fc9187
horquidia-lima <horquidia.a.lima@gmail.com>
2024-01-16 17:38:30 -0300

    atualizando usuario

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -37,6 +37,38 @@ class UsersController {
     //response.send(`Usuario ${name} - Email: ${email} e a senha ${password}`)
     //response.json({name, email, password})
   }
+
+  async update(request, response) {
+    const {name, email} = request.body
+    const {id} = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    if(!user){
+      throw new AppError("Usuario nao encontrado")
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError("Este e-mail ja esta em uso")
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+      )
+
+      return response.json()
+  }
 }
 
 module.exports = UsersController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,17 +4,18 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
-function myMiddleware(request, response, next){
+/*function myMiddleware(request, response, next){
   console.log("Voce passou pelo Middleware")
   if(!request.body.isAdmin){
     return response.json({message: "user unauthorized"})
   }
   next()
-}
+}*/
 
 
-const usersController = new UsersController //instaciando a class (guardando na memoria)
+const usersController = new UsersController() //instaciando a class (guardando na memoria)
 
-usersRoutes.post("/", myMiddleware, usersController.create)
+usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:id", usersController.update)
 
 module.exports = usersRoutes
