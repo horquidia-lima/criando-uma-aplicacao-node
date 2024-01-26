f159c15e2f17ddf591d3b03a0bf32950fb5987fd
horquidia-lima <horquidia.a.lima@gmail.com>
2024-01-25 21:49:48 -0300

    Deletando nota

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -17,5 +17,6 @@ const notesController = new NotesController() //instaciando a class (guardando n
 
 notesRoutes.post("/:user_id", notesController.create)
 notesRoutes.get("/:id", notesController.show)
+notesRoutes.delete("/:id", notesController.delete)
 
 module.exports = notesRoutes
